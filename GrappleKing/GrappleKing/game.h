33832d266a00ae0d07fc683669d71e6595dbeb89
ysyudai <ys1139u@gmail.com>
2025-02-12 19:00:10 +0900

    up

---
+++ GrappleKing/GrappleKing/game.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+namespace Game
+{
+	constexpr int kScreenWidth = 1280;
+	constexpr int kScreenHeight = 720;
+	constexpr int kScreenBitNum = 32;
+	constexpr bool kDefaultWindowMode = false;
+}
--- GrappleKing_20241125_シーン遷移/GrappleKing/GrappleKing/game.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-namespace Game
-{
-	constexpr int kScreenWidth = 1280;
-	constexpr int kScreenHeight = 720;
-	constexpr int kScreenBitNum = 32;
-	constexpr bool kDefaultWindowMode = true;
-}
